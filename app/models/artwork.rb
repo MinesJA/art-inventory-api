127a1ef3a2be4dc2a968c3957be66dfd311999f4
MinesJA <minesja@gmail.com>
2020-04-30 08:18:43 -0400

    Finished artist and artwork models

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,4 +2,10 @@ class Artwork < ApplicationRecord
   belongs_to :artist
 
   validates_presence_of :title
+  validates_presence_of :height_in
+  validates_presence_of :width_in
+
+  def started_at
+    self[:started_at] || self[:created_at]
+  end
 end
--- spec/models/artwork_spec.rb
+++ spec/models/artwork_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe Artwork, type: :model do
   it { should belong_to(:artist) }
 
   it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:commission) }
-  it { should validate_presence_of(:date_started) }
   it { should validate_presence_of(:height_in) }
   it { should validate_presence_of(:width_in) }
+
+  it 'should default commission to true' do
+    artwork = Artwork.create(title: "Title", height_in: 12.00, width_in: 12.00)
+    expect(artwork.commission).to eq(false)
+  end
+
+  it 'should return created_at date if date_started is nil' do
+    artwork = Artwork.create(title: "Title", height_in: 12.00, width_in: 12.00)
+    # artwork.date_started.should == artwork.created_at
+    expect(artwork.date_started).to eq(artwork.created_at)
+  end
 
 end
